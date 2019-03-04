e318be9c304aafb14c519f53b36626856c32e0cc
UmmeFerdousi <47343676+UmmeFerdousi@users.noreply.github.com>
2019-03-03 21:12:20 -0500

    Add files via upload

---
+++ task02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import sys
+sys.path.append("C:\\Users\\Mou\\Desktop\\School\\Spring19\\IS210\\Week5")
+import task01.peanut
+TIME= task01.peanut.BUTTER
+
---
+++ task03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import sys
+sys.path.append("C:\\Users\\Mou\\Desktop\\School\\Spring19\\IS210\\Week5")
+import task01.peanut
+import copy
+BUTTER=copy.copy(task01.peanut.BUTTER)
+JELLY=BUTTER
+
